be40b5674e8070b5d33f3babdbe9a843325f668c
Beuuuh <bellmarques8768@hotmail.com>
2022-08-21 09:12:43 -0300

    calculator exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeater = num
+    let phrase = ''
+    for(i = 0; i <= num; i++) {
+        phrase = string + string
+    }
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a, b) {
+	return a + b
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for(i = 1; i < a; i++) {
+    total *= i
+  };
+  return total;
 };
 
 // Do not edit below this line
